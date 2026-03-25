64207d86ff634803dc1d9cf242e0a100b5f9cac2
João Keweni S R <joaokeweni49@gmail.com>
2026-03-25 20:02:15 -0300

    Criação do menu inicial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,40 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Seja bem vindo ao BuscaCEP");
+        Scanner leitura = new Scanner(System.in);
+        int opcao = 0;
+        double cep = 0;
+        do {
+            System.out.println("1- Buscar pelo CEP");
+            System.out.println("2- Baixar JSON");
+            System.out.println("3- Sair");
+            System.out.println("Escolhar uma das opções abaixo: ");
+            opcao = leitura.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            switch (opcao){
+                case 1:
+                    System.out.println("Digite o CEP: ");
+                    cep = leitura.nextDouble();
+                    //TODO: criar metodo de buscar o cep usando cep digitado
+                    break;
+                case 2:
+                    System.out.println("Digite o CEP: ");
+                    cep = leitura.nextDouble();
+                    //TODO: Usar o metodo de buscar o cep
+                    //TODO: Criar o metodo de criar o JSON com os dados retornados da minha API
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+
+            }
+        }while (opcao != 3);
+
     }
 }
